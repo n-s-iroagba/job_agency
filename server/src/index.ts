4d9d90e7cbd5f85be0a65d0dce673f0f6b539b2c
n-s-iroagba <nnamdisolomon1@gmail.com>
2026-04-17 02:42:09 +0100

    fix production bugs

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,11 +7,10 @@ import './models';
 
 
 const PORT = process.env.PORT || 3000;
-       
+
 const startServer = async () => {
     try {
         await connectDB();
-await sequelize.sync({ alter: true });
 
         if (process.env.NODE_ENV !== 'production') {
             //await sequelize.sync({ force: true });
